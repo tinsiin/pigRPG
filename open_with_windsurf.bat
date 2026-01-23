1ea5b9821e64b1c21d58f1bc7b12bc76bc15db12
tinsiin <78485267+tinsiin@users.noreply.github.com>
2026-01-23 17:51:19 +0900

    計画追記とbatいらんの削除

--- open_with_windsurf.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "WINDSURF_PATH=C:\Users\teinshiiin\AppData\Local\Programs\Windsurf\Windsurf.exe"
-set "WORKSPACE_PATH=C:\Users\teinshiiin\Documents\GitHub\pigRPG\pigRPG.code-workspace"
-"%WINDSURF_PATH%" "%WORKSPACE_PATH%"
-exit /b 0
